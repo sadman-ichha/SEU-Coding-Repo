431151c7e9fbd388bfb6c169a9f3259009e3571c
Sadman Ichha <sadmanichha1@gmail.com>
2025-03-12 22:46:07 +0600

    added:array oparation - searching,insert

--- Data-Structure-Algorithms/ds/01_array/array.cpp
+++ Data-Structure-Algorithms/ds/01_array/array.cpp
@@ -4,31 +4,64 @@ using namespace std;
 int main()
 {
     // Traverse Array using loop
-     int orange[7] = {3, 4, 12, 7, 8, 50};
-    for (int i = 0; i < 7; i += 1){
+    int orange[7] = {3, 4, 12, 7, 8, 50};
+    cout<<endl<<"array element: ";
+    for (int i = 0; i < 7; i += 1)
+    {
         cout << orange[i];
         // To ensure that a comma (",") is printed after each element except the last one
-        if (i != 6){
+        if (i != 6)
+        {
             cout << ",";
         }
     }
 
-     // Update Oparation
-     orange[2] = 13;
+    // Update Oparation && printing 
+    orange[2] = 13;
+    cout<<endl<<"Update array element: ";
+    for (int i = 0; i < 7; i += 1)
+    {
+        cout<< orange[i];
+        if (i != 6)
+        {
+            cout << ",";
+        }
+    }
+    cout << endl
+         << endl;
 
+    // Searching the element
+    cout << "Find The Number: ";
+    int findNumber;
+    bool isFound = false;
+    cin >> findNumber;
+    for (int i = 0; i < 7; i++)
+    {
+        if (findNumber == orange[i])
+        {
+            cout << "your number is find the index[" << i << "]: " << findNumber
+                 << endl;
+            isFound = true;
+            break;
+        }
+    }
+    if (!isFound)
+    {
+        cout << "Number not found!!" << endl;
+    }
 
-    cout<<endl<<endl;
-
-    // Insert Oparation 
+    // Insert Oparation
     int apple[5];
-    for(int i=0;i<5;i+=1){
-        cout<<"Insert ["<<i<<"] number: ";
-        cin>>apple[i];
+    cout << "Please enter 5 integer numbers:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << "Insert number at index [" << i << "]: ";
+        cin >> apple[i];
     }
-
-    for(int i=0;i<5;i+=1){
-        cout<<apple[i]<<" ";
+    cout << "\nYou entered: ";
+    for (int i = 0; i < 5; i++) {
+        cout << apple[i] << " ";
     }
+    cout << endl;
 
     return 0;
 }
