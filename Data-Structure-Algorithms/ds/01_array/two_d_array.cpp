ae5fc62ffe305580e648e9ef5293b631a08f7b79
Sadman Ichha <sadmanichha1@gmail.com>
2025-03-13 21:33:52 +0600

    Added: 2D array initialization, user input, and improved formatting

--- Data-Structure-Algorithms/ds/01_array/two_d_array.cpp
+++ Data-Structure-Algorithms/ds/01_array/two_d_array.cpp
@@ -2,8 +2,46 @@
 using namespace std;
 
 int main() {
-    
-    
+
+    // Declaring 2D array
+     int apple[2][3]; // here [2] is row and [3] is column represents (2x3 array)
+
+    // Row (1)
+    apple[0][0]= 10;
+    apple[0][1]= 20;
+    apple[0][2]= 30;
+    // Row (2)
+    apple[1][0]= 40;
+    apple[1][1]= 50;
+    apple[1][2]= 60;
+   
+    // Output loop
+    cout << "\nThe 2D array is:\n";
+    for(int i= 0; i<2; i+=1){
+        for(int j =0; j<3;j++ ){
+           cout<<apple[i][j]<<" ";
+        }
+    }
+
+
+    int orange[3][3]; //Row 3 coloumn 3 (2x3 array)
+    cout << "\nPlease input your 2D Array (2 rows, 3 columns): "<<endl;
+    for (int i = 0; i < 3; i+=1) // (i) represents the row 
+    {
+        for (int j = 0; j < 3; j++) // (j) represents the colums
+        {
+            cout << "Index at [" << i << "][" << j << "]: ";
+            cin>>orange[i][j];
+        }
+    }
+    // Output loop
+    cout << "\nThe 2D array is:\n";
+    for(int i= 0; i<3; i+=1){
+        for(int j =0; j<3;j++ ){
+            cout<<orange[i][j]<<" ";
+        }
+    }
+
     
     return 0;
 }
