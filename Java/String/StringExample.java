bd43752fed11e1f2a55946463bb7ff0dc7780924
Sadman Ichha <sadmanichha1@gmail.com>
2025-03-15 23:09:56 +0600

    Added: Java

--- Java/String/StringExample.java
+++ Java/String/StringExample.java
@@ -1,10 +1,21 @@
+import java.util.Date;
 
 public class StringExample {
     public static void main(String[] args) {
         String name = "Sadman";
+        String a = new String("Hello World!");
+        char[] helloArray = { 'H', 'E', 'L', 'L', 'O' };
+        System.out.println(helloArray);
+        byte[] b = new byte[] { 97, 98, 99 };
+        System.out.println("b: " + new String(b));
+        Date date = new Date();
+
+
         System.out.println(name.length());
-
-
+        String t = "Please locate where 'locate' occurs!";
+        System.out.println(t.indexOf("locate"));
+        System.out.println(t.charAt(4));
+        System.out.println(t.compareTo(name));
     }
 
 }
