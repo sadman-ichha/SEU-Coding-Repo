ea574dc1cefa76e5f2a20d24fa779b0447e18559
Sadman Ichha <sadmanichha1@gmail.com>
2025-03-24 23:41:20 +0600

    added

--- OOPs-CPP/03. Inheritance/inheritance.cpp
+++ OOPs-CPP/03. Inheritance/inheritance.cpp
@@ -1,36 +1,60 @@
 #include <iostream>
 using namespace std;
 
-
 // Base class Person
-class Person {
-    public:
-        string name; 
-        int age;   
-        
-       
-    };
-    
-    // Derived class Student inheriting from Person
-    class Student : public Person {
-    public:
-        int rollNo; // Additional member variable specific to Student
+class Person
+{
+public:
+    string name;
+    int age;
 
-        void getInfo() {
-            cout << "Name: " << name << endl;  
-            cout << "Age: " << age << endl;   
-            cout << "Roll No: " << rollNo << endl; // Corrected label from "Age" to "Roll No"
-        } 
-    };
+    // Constructor of Person class
+    Person()
+    {
+        cout << "Called Person Constructor..\n"; // Message when a Person object is created
+    }
 
+    // Destructor of Person class
+    ~Person()
+    {
+        cout << "Called Person Destructor..\n"; // Message when a Person object is destroyed
+    }
+};
 
-int main() {
+// Derived class Student inheriting from Person
+class Student : public Person
+{
+public:
+    int rollNo; // Additional member variable specific to Student
+
+    // Constructor of Student class
+    Student()
+    {
+        cout << "Called Student Constructor..\n"; // Message when a Student object is created
+    }
+
+    // Destructor of Student class
+    ~Student()
+    {
+        cout << "Called Student Destructor..\n"; // Message when a Student object is destroyed
+    }
+
+    void getInfo()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Roll No: " << rollNo << endl; //Corrected label from "Age" to "Roll No"
+    }
+};
+
+int main()
+{
 
     Student s1;
-    s1.name ="Sadman";
-    s1.age =123;
-    s1.rollNo =1;
+    s1.name = "Sadman";
+    s1.age = 123;
+    s1.rollNo = 1;
     s1.getInfo();
-    
+
     return 0;
 }
