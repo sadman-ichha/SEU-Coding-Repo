0d16c16041b60f22ea7365e3f80abe6fec00d55c
Sadman Ichha <sadmanichha1@gmail.com>
2025-03-25 21:22:53 +0600

    OOPs-CPP Added: multiple inheritance

---
+++ OOPs-CPP/03. Inheritance/multiple_inheritance.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Person {
+public:
+    string name;
+    int age;
+}; 
+
+// Derived class from Person
+class Student : public Person {
+public:
+    int rollNo;
+}; 
+
+// Derived class from Student (Multi-level Inheritance)
+class Alumni : public Student {
+public:
+    void getInfo() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Roll No: " << rollNo << endl;
+    }
+}; 
+
+int main() {
+    Alumni a1;
+    a1.name = "Sadman";
+    a1.age = 12;
+    a1.rollNo = 123;
+
+    a1.getInfo();
+
+    return 0;
+}
