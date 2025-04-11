a077b6a0cf4231089ebbd171243648903373b796
Sadman Ichha <sadmanichha1@gmail.com>
2025-04-11 22:13:33 +0600

    Added: Sorting algo - bubble sort

--- Data-Structure-Algorithms/algo/Sorting-Alogs/bubble_sort.cpp
+++ Data-Structure-Algorithms/algo/Sorting-Alogs/bubble_sort.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
+int main()
+{
+    int arr[] = {5, 3, 8, 1, 4};
+    int length = 5;
+    cout << "Unsorted array: ";
+    for (int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+    // algorithm 
+    for (int i = 0; i < length; i++){
+        for (int j = 0; j < length-1; j++){
+            if (arr[j] > arr[j + 1]){
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+
+    cout <<endl<< "Sorted array: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
