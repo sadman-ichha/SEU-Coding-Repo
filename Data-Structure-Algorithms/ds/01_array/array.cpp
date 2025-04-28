2a0a817a8540815f9da1d6cec786b197e1fd86ca
Sadman Ichha <sadmanichha1@gmail.com>
2025-04-28 23:49:03 +0600

    added

--- Data-Structure-Algorithms/ds/01_array/array.cpp
+++ Data-Structure-Algorithms/ds/01_array/array.cpp
@@ -14,7 +14,7 @@ int main()
     for (int i = 0; i < arrLength; i += 1)
     {
         cout << orange[i];
-        // To ensure that a comma (",") is printed after each element except the last one one
+        // To ensure that a comma (",") is printed after each element except the last one
         if (i != 6)
         {
             cout << ",";
