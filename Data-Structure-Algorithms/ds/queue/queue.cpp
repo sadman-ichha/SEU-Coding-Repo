2fdcf121adcb61a1d1fd2175d2bc0d88b4674f38
Sadman Ichha <sadmanichha1@gmail.com>
2025-05-13 22:22:26 +0600

    added:queue

--- Data-Structure-Algorithms/ds/queue/queue.cpp
+++ Data-Structure-Algorithms/ds/queue/queue.cpp
@@ -2,9 +2,42 @@
 
 using namespace std;
 
-class 
+class Queue{
+  private:
+    int arr[2];
+    int rear, front;
+ 
+  public:
+    Queue(){
+     front = -1;
+     rear = -1;
+    }
+
+    void enqueue(int value){
+        rear++;
+        if(front==-1) front =0;
+        arr[rear] = value;
+        cout<<"enqueue: "<< value<<endl;
+    }
+
+    int dequeue(){
+        cout<<"dequeue: "<< arr[front]<<endl;
+        if(front==0 || front>rear){
+            cout << "Queue Underflow" << endl;
+            return -1;
+        }  
+        return arr[front++];
+    }
+
+
+};
 
 int main(){
+    Queue queue;
+    queue.enqueue(12);
+    queue.enqueue(11);
+    queue.dequeue();
+    queue.dequeue();
 
 
     return 0;
--- Data-Structure-Algorithms/ds/stack/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-stack.push(1);
-    // stack.push(2);
-    // stack.push(3);
